2c0a120b16217b686c56a4c836601d81c5441412
chitose <1009950744@qq.com>
2018-09-29 20:07:08 +0800

    Site updated: 2018-09-29 20:07:06

--- js/music.js
+++ js/music.js
@@ -11,6 +11,12 @@ const ap = new APlayer({
         cover: 'http://p2.music.126.net/J1K37kRTg8GNQ6cCYhvuWQ==/5899979394913533.jpg',
       },
       {
+        name: "rain",
+        artist: 'nekoboro',
+        url: 'http://music.163.com/song/media/outer/url?id=29713218.mp3',
+        cover: 'http://p1.music.126.net/XQ0apoeD66MvCdhpcBtByg==/3239161255748471.jpg',
+      },
+      {
         name: 'eau',
         artist: 'yutaka hirasaka',
         url: 'http://music.163.com/song/media/outer/url?id=29535887.mp3',
@@ -21,6 +27,12 @@ const ap = new APlayer({
         artist: 'haruka nakamura',
         url: 'http://music.163.com/song/media/outer/url?id=4904298.mp3',
         cover: 'http://p2.music.126.net/sM0gbClGYFr4r6NXy_77_Q==/857619069688801.jpg',
+      },
+      {
+        name: "alone,,again",
+        artist: 'elijah who',
+        url: 'http://music.163.com/song/media/outer/url?id=495218039.mp3',
+        cover: 'http://p2.music.126.net/qJRzJOtNhcPlkTaljwI-0w==/19106213556171653.jpg',
       }
     ]
 });
